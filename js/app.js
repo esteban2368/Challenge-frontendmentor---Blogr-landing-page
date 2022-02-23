977f5d5d2b7ffa2b7263bf6ed7f74e339146fef2
esteban2368 <metalbrayan1968@hotmail.com>
2022-02-22 19:53:32 -0500

    Terminación de funcionalidad de menú responsive y botón

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
 
 window.addEventListener("DOMContentLoaded",() =>{
     const btnMenu = document.querySelector(".button--navbar");
+    const navbarResponsive = document.querySelector(".navbar-responsive");
+
     btnMenu.addEventListener("click", (e) =>{
+        
         e.currentTarget.classList.toggle("open");
+        navbarResponsive.classList.toggle("navbar-responsive--visible");
+
     })
 })
